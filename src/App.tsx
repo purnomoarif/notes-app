e627de1e2db526f6961619aa5779b118de6471b6
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-08-02 10:35:37 +0700

    feat: conditional rendereing for  begginner

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ export function App() {
     <div className="App">
       <h1>Notes App</h1>
       <button>Click Me</button>
-      <Notes name="Day 01 Bearmentor" />
-      <Notes name="Day 02 Bearmentor" />
-      <Notes name="Day 03 Bearmentor" />
-      <Notes name="Day 04 Bearmentor" />
-      <Notes name="Day 05 Bearmentor" />
+      <Notes name="Day 01 Bearmentor" isIntroduction={true} />
+      <Notes name="Day 02 Bearmentor" isIntroduction={false} />
+      <Notes name="Day 03 Bearmentor" isIntroduction={false} />
+      <Notes name="Day 04 Bearmentor" isIntroduction={false} />
+      <Notes name="Day 05 Bearmentor" isIntroduction={false} />
     </div>
   );
 }
 
-export function Notes({ name }: { name: string }) {
+export function Notes({
+  name,
+  isIntroduction,
+}: {
+  name: string;
+  isIntroduction: boolean;
+}) {
   return (
     <div>
-      <h2>{name}</h2>
+      <h2>{name} 🔰</h2>
+      {isIntroduction && <h3>Introduction</h3>}
     </div>
   );
 }
