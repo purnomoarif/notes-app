6eea44e5c3222acc5be219b368f2a3afd48ad2fe
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-08-03 16:56:53 +0700

    feat: add notes data structure and update rendering logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,72 @@
+const dataNotes = [
+  { id: 1, name: 'Day 01: Introduction', isDone: true },
+  { id: 2, name: 'Day 02: UI/UX Design', isDone: true },
+  { id: 3, name: 'Day 03: Code Editor', isDone: true },
+  { id: 4, name: 'Day 4: Terminal, Github', isDone: true },
+  { id: 5, name: 'Day 5: HTML, Domain', isDone: true },
+  { id: 6, name: 'Day 6: CSS Layout', isDone: true },
+  { id: 7, name: 'Day 7: Framework, Tailwind CSS', isDone: true },
+  {
+    id: 8,
+    name: 'Day 8: Website Project Presentation and Evaluation',
+    isDone: true,
+  },
+
+  { id: 9, name: 'Day 9: Algorithm, Data Structure', isDone: true },
+  {
+    id: 10,
+    name: 'Day 10: JavaScript, Console, Variables, Types',
+    isDone: true,
+  },
+  { id: 11, name: 'Day 11: Loops, Function', isDone: true },
+  { id: 12, name: 'Day 12: Globals/Properties/Methods', isDone: true },
+  {
+    id: 13,
+    name: 'Day 13: Storage, Async, Web API, Data Fetching',
+    isDone: true,
+  },
+  { id: 14, name: 'Day 14: DOM (Document Object Model)', isDone: true },
+  { id: 15, name: 'Day 15: OOP', isDone: true },
+  { id: 16, name: 'Day 16: Project Presentation and Evaluation', isDone: true },
+  {
+    id: 17,
+    name: 'Day 17: JavaScript Modules, Runtime, Package Manager, Bundler, TypeScript',
+    isDone: true,
+  },
+  {
+    id: 18,
+    name: 'Day 18: JavaScript Library/Framework, React, Components, Props',
+    isDone: true,
+  },
+  { id: 19, name: 'Day 19: React Styling, React UI Components', isDone: true },
+  { id: 20, name: 'Day 20: React Hooks, React State', isDone: false },
+]
+
 export function App() {
   return (
     <div className="App">
       <h1>Notes App</h1>
-      <button>Click Me</button>
-      <Notes name="Day 01 Bearmentor" isIntroduction={true} />
-      <Notes name="Day 02 Bearmentor" isIntroduction={false} />
-      <Notes name="Day 03 Bearmentor" isIntroduction={false} />
-      <Notes name="Day 04 Bearmentor" isIntroduction={false} />
-      <Notes name="Day 05 Bearmentor" isIntroduction={false} />
+      <ul>
+        {dataNotes.map((note) => {
+          return (
+            <li key={note.id}>
+              <Notes name={note.name} isDone={note.isDone} />
+            </li>
+          )
+        })}
+      </ul>
     </div>
-  );
+  )
 }
 
-export function Notes({
-  name,
-  isIntroduction,
-}: {
-  name: string;
-  isIntroduction: boolean;
-}) {
+export function Notes({ name, isDone }: { name: string; isDone: boolean }) {
   return (
     <div>
-      <h2>{name} 🔰</h2>
-      {isIntroduction && <h3>Introduction</h3>}
+      <h2>
+        {name} {isDone && <span>✅</span>}
+      </h2>
+
+      {!isDone && <h3>Not done yet 📝</h3>}
     </div>
-  );
+  )
 }
