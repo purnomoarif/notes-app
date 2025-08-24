bbf5a261ffdc32ba4ec8125899e55f28de832f4d
Arif Purnomo <purnomoarifdepok@gmail.com>
2025-08-24 16:42:08 +0700

    feat: get note by id

--- src/routes/note-id.tsx
+++ src/routes/note-id.tsx
@@ -1,3 +1,4 @@
+import type { DataNote } from '@/modules/note/data'
 import { useParams } from 'react-router'
 
 export function NoteId() {
@@ -5,11 +6,26 @@ export function NoteId() {
 
   console.log(params)
 
-  const id = params.id
+  const id = Number(params.id)
+
+  const storedNotes = localStorage.getItem('notes')
+  const notes = JSON.parse(storedNotes) as DataNote[]
+
+  const note = notes.find((note) => note.id === id)
+  console.log(note)
+
+  if (!note) {
+    return (
+      <div>
+        <p>Sorry, we couldn't find the note you're looking for.</p>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h1>Note: {id}</h1>
+      <h1> {note.name}</h1>
+      <p> {note.description}</p>
     </div>
   )
 }
